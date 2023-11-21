f89bd663c847ee7bfd6e6345c395e311b2cbb962
Ruslan Shablia <generalov-rus@mail.ru>
2023-11-21 14:46:33 +0200

    hw 1

---
+++ hw1/script.js
@@ -0,0 +1,54 @@
+//task1
+
+let str1 = 'hello';
+console.log(str1);
+let str2 = "owu";
+console.log(str2);
+let str3 = "com";
+console.log(str3);
+let str4 = "ua"
+console.log(str4);
+let num1 = 1;
+console.log(num1);
+let num2 = 10;
+console.log(num2);
+let num3 = -999;
+console.log(num3);
+let num4 = 123;
+console.log(num4);
+let num5 = 3.14;
+console.log(num5);
+let num6 = 2.17;
+console.log(num6);
+let num7 = 16;
+console.log(num7);
+let bool = true;
+console.log(bool)
+let math = false;
+console.log(math);
+
+console.log('-----------');
+
+//task2
+
+let firstName = "Ruslan";
+let middleName = "Andriyovych";
+let lastName = "Shablia";
+
+let person = (lastName + ' ' + firstName + ' '+  middleName);
+console.log(person);
+
+console.log('-----------');
+
+//task3
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a + ' ' + typeof b + ' ' + typeof c);
+
+
+
+
+
